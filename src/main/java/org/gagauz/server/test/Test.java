16352068d6b94910e938dd19818c4cfa069b5621
miga <miga@wolf5>
2015-04-01 21:45:46 +0300

    1

--- src/main/java/org/gagauz/server/test/Test.java
+++ src/main/java/org/gagauz/server/test/Test.java
@@ -12,35 +12,6 @@ public class Test {
         s.setDocumentRoot("c:\\Users\\java\\apache-tomcat-6.0.41\\webapps");
         s.setCharset(Charset.forName("utf-8"));
         s.start(8099);
-        // Socket st;
-        // try {
-        // st = new Socket("yandex.ru", 80);
-        // System.out.println("connect...");
-        // while (true) {
-        // if (st.isConnected()) {
-        // System.out.println("connected");
-        //
-        // st.getOutputStream()
-        // .write(("GET / HTTP/1.1\r\n"
-        // +
-        // "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n"
-        // +
-        // "Connection:keep-alive\r\n"
-        // +
-        // "Host:google.com\r\n"
-        // +
-        // "User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36\r\n"
-        // +
-        // "\r\n").getBytes());
-        // st.getOutputStream().flush();
-        // while (st.getInputStream().available() > 0) {
-        // System.out.write(st.getInputStream().read());
-        // }
-        // }
-        // }
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // }
 
     }
 }
